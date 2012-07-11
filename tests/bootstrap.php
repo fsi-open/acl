6bb4b2229096d8eb11fb904caa2c591bf176f7bb
Norbert Orzechowicz <norbert.orzechowicz@fsi.pl>
2012-07-11 10:43:56 +0200

    Added composer autoload support to tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * This is bootstrap for phpUnit unit tests,
- * use README.md for more details
- *
- * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
- * @package Gedmo.Tests
- * @link http://www.gediminasm.org
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
+
+error_reporting(E_ALL | E_STRICT);
 
 if (!class_exists('PHPUnit_Framework_TestCase') ||
     version_compare(PHPUnit_Runner_Version::id(), '3.5') < 0
@@ -18,21 +11,10 @@ if (!class_exists('PHPUnit_Framework_TestCase') ||
 if (!class_exists('PHPUnit_Framework_MockObject_MockBuilder')) {
     die('PHPUnit MockObject plugin is required, at least 1.0.8 version');
 }
-
-define('TESTS_PATH', __DIR__);
-define('TESTS_TEMP_DIR', __DIR__.'/temp');
-define('VENDOR_PATH', realpath(__DIR__ . '/../vendor'));
-
-$classLoaderFile = VENDOR_PATH . '/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-if (!file_exists($classLoaderFile)) {
-    die('cannot find vendor, run: php bin/vendors.php');
+if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
+    die('Install vendors using command: composer.phar install');    
 }
-require_once $classLoaderFile;
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader;
-$loader->registerNamespaces(array(
-    // imported namespaces
-    'Symfony'                    => VENDOR_PATH,
-    // FSi namespaces
-	'FSi\\Component\\ACL'            => __DIR__.'/../lib',
-));
-$loader->register();
+
+$loader = require_once __DIR__.'/../vendor/autoload.php';
+$loader->add('FSi\\Component\\ACL', __DIR__);
+
